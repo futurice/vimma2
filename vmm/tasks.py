374fa7d9dbebdd16b1fdda233ea055df5e5bd1f9
Markus Koskinen <markus.koskinen@futurice.com>
2014-02-19 16:50:23 +0200

    adding some model handling to the creation task

--- vmm/tasks.py
+++ vmm/tasks.py
@@ -1,10 +1,14 @@
 from __future__ import absolute_import
 
 import os
+import time
 
 from celery import Celery
-from celery import shared_task
+
+#from celery import shared_task
 from django.conf import settings
+
+from vmm.models import VirtualMachine
 
 import aws.create_ami
 
@@ -13,11 +17,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vimma2.settings')
 app = Celery('tasks')
 app.config_from_object('django.conf:settings')
 
-#@app.task
-#def add(x, y):
-#    return "celery: "+ x + y
-
 @app.task
 def create_vm():
+    vm_name = "demovm" + str(int(time.time()))
+    vm_obj = VirtualMachine(primary_name = vm_name, schedule_id = 1)
+    vm_obj.save()
     vm_dict = aws.create_ami.main()
+    #vm_dict = {}
     return vm_dict
