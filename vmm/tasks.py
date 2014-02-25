ce505e0b3364c84cc0cd4ba20bb79c1d6b3dff1c
Markus Koskinen <markus.koskinen@futurice.com>
2014-02-25 17:45:07 +0200

    small changes in tasks

--- vmm/tasks.py
+++ vmm/tasks.py
@@ -21,16 +21,20 @@ logger = get_task_logger(__name__)
 
 @app.task
 def create_vm():
+    logger.warning('Starting to create instance.')
     aws_conn = aws.AWS_conn.EC2Conn()
     aws_conn.connect()
 
     vm_name = "demovm" + str(int(time.time()))
+    logger.warning('Creating instance with name: %r' % vm_name)
+
     vm_obj = VirtualMachine(primary_name = vm_name, schedule_id = 1)
     vm_obj.save()
 
     try:
         vm_dict = aws_conn.create_instance(instance_name=vm_name).__dict__
     except:
+        logger.warning('Instance creation failed for instance name: %r' % vm_name)
         VirtualMachine.objects.filter(primary_name=vm_name).delete()
         return dict()
     else:
@@ -47,8 +51,9 @@ def terminate_vm(instance_id):
 
     try:
         aws_conn.terminate_instance(instance_id=instance_id)
-        VirtualMachine.objects.filter(instance_id=instance_id).delete()
     except:
+        logger.warning('Instance termination failed for instance id: %r' % instance_id)
         return False
     else:
+        VirtualMachine.objects.filter(instance_id=instance_id).delete()
         return True
