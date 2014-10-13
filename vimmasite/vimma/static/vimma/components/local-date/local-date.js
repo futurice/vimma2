d3a9fccc8156ccd8b7064681d6240d61c87d0e36
Mihai <mihaiftr@users.noreply.github.com>
2014-10-13 18:19:14 +0300

    add basic <local-date> elem

---
+++ vimmasite/vimma/static/vimma/components/local-date/local-date.js
@@ -0,0 +1,35 @@
+Polymer('local-date', {
+    date: '',
+
+    dateStr: 'YYYY-MM-DD',
+    showDate: true,
+    timeStr: 'HH:MM:SS.mmm',
+    isoStr: '', // can't find a way to show this as <some-elem title=…>
+
+    ready: function() {
+        this.dateObj = new Date(this.date);
+        var now = new Date();
+
+        if (now.getFullYear() == this.dateObj.getFullYear() &&
+                now.getMonth() == this.dateObj.getMonth() &&
+                now.getDate() == this.dateObj.getDate()) {
+            this.showDate = false;
+        }
+
+        function fmtInt(i) {
+            var s = i + '';
+            if (s.length == 1) {
+                s = '0' + s;
+            }
+            return s;
+        }
+
+        this.dateStr = [this.dateObj.getFullYear(), this.dateObj.getMonth(),
+            this.dateObj.getDate()].map(fmtInt).join('-');
+        this.timeStr = [this.dateObj.getHours(), this.dateObj.getMinutes(),
+            this.dateObj.getSeconds()].map(fmtInt).join(':') + '.' +
+                this.dateObj.getMilliseconds();
+
+        this.isoStr = this.dateObj.toISOString();
+    }
+});
