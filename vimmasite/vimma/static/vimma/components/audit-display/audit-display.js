3a61221bf8d37a0c6321dfcbfa36c151c0aec174
Mihai <mihaiftr@users.noreply.github.com>
2014-10-23 18:06:30 +0300

    show username in <audit-display/> component

--- vimmasite/vimma/static/vimma/components/audit-display/audit-display.js
+++ vimmasite/vimma/static/vimma/components/audit-display/audit-display.js
@@ -33,6 +33,8 @@ Polymer('audit-display', {
     pageSize: 5,
     // can't refer to ‘restMaxPaginateBy’ in the template disabled?= binding
     maxPageSize: restMaxPaginateBy,
+    // {userId: userObject, …}
+    usersById: null,
     // API endpoint response {count:, next:, previous:, results:}
     apiResult: null,
     firstItemNr: 1,
@@ -47,9 +49,10 @@ Polymer('audit-display', {
         this.loading = true;
 
         // don't reset the page size
+        this.usersById = null;
         this.apiResult = null;
         this.firstItemNr = 1;
-        this.loadFirstPage();
+        this.loadUsers();
     },
     loadFail: function(errorText) {
         this.fire('ajax-end', {success: false, errorText: errorText});
@@ -62,6 +65,19 @@ Polymer('audit-display', {
 
         this.loading = false;
         this.loadingSucceeded = true;
+    },
+
+    loadUsers: function() {
+        var ok = (function(arr) {
+            var byId = {};
+            arr[0].forEach(function(u) {
+                byId[u.id] = u;
+            });
+            this.usersById = byId;
+
+            this.loadFirstPage();
+        }).bind(this);
+        apiGetAll([vimmaApiUserList], ok, this.loadFail.bind(this));
     },
 
     loadFirstPage: function() {
@@ -78,39 +94,31 @@ Polymer('audit-display', {
             url += '?' + params.join('&');
         }
 
-        $.ajax({
-            url: url,
-            success: (function(data) {
-                this.apiResult = data;
-                this.firstItemNr = this.apiResult.count ? 1 : 0;
-                this.loadSuccess();
-            }).bind(this),
-            error: (function() {
-                var errorText = getAjaxErr.apply(this, arguments);
-                this.loadFail(errorText);
-            }).bind(this)
-        });
+        var ok = (function(arr) {
+            this.apiResult = arr[0];
+            this.firstItemNr = this.apiResult.count ? 1 : 0;
+            this.loadSuccess();
+        }).bind(this);
+        apiGet([url], ok, this.loadFail.bind(this));
     },
 
     loadUrl: function(url, go_right) {
         this.fire('ajax-start');
         var first_right = this.firstItemNr + this.apiResult.results.length;
 
-        $.ajax({
-            url: url,
-            success: (function(data) {
-                this.apiResult = data;
+        var ok = (function(arr) {
+            this.apiResult = arr[0];
 
-                var first_left = this.firstItemNr - this.apiResult.results.length;
-                this.firstItemNr = go_right ? first_right : first_left;
+            var first_left = this.firstItemNr - this.apiResult.results.length;
+            this.firstItemNr = go_right ? first_right : first_left;
 
-                this.fire('ajax-end', {success: true});
-            }).bind(this),
-            error: (function() {
-                var errorText = getAjaxErr.apply(this, arguments);
-                this.fire('ajax-end', {success: false, errorText: errorText});
-            }).bind(this)
-        });
+            this.fire('ajax-end', {success: true});
+        }).bind(this);
+        var fail = (function(errorText) {
+            this.fire('ajax-end', {success: false, errorText: errorText});
+        }).bind(this);
+
+        apiGet([url], ok, fail);
     },
     loadPrevious: function() {
         this.loadUrl(this.apiResult.previous, false);
@@ -122,5 +130,15 @@ Polymer('audit-display', {
     showMore: function() {
         this.pageSize *= 2;
         this.reload();
+    },
+
+    getUserText: function(userId) {
+        if (userId == null) {
+            return '—';
+        }
+        if (!(userId in this.usersById)) {
+            return userId + '?';
+        }
+        return this.usersById[userId].username;
     }
 });
