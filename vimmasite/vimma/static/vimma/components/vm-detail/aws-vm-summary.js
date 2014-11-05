df482ecb119f436613c855ab6f690681c84f3a31
Mihai <mihaiftr@users.noreply.github.com>
2014-11-05 15:42:07 +0200

    show vm summary line

---
+++ vimmasite/vimma/static/vimma/components/vm-detail/aws-vm-summary.js
@@ -0,0 +1,45 @@
+Polymer('aws-vm-summary', {
+    /* same as for <vm-detail> */
+    loading: true,
+    loadingSucceeded: false,
+
+    vm: null,
+    awsvm: null,
+
+    attached: function() {
+        this.reload();
+    },
+
+    reload: function() {
+        this.$.ajax.fire('start');
+        this.loading = true;
+
+        this.vm = null;
+        this.awsvm = null;
+
+        this.loadVM();
+    },
+    loadFail: function(errorText) {
+        this.$.ajax.fire('end', {success: false, errorText: errorText});
+
+        this.loading = false;
+        this.loadingSucceeded = false;
+    },
+    loadSuccess: function() {
+        this.$.ajax.fire('end', {success: true});
+
+        this.loading = false;
+        this.loadingSucceeded = true;
+    },
+
+    loadVM: function() {
+        var ok = (function(resultArr) {
+            this.vm = resultArr[0];
+            this.awsvm = resultArr[1].results[0];
+            this.loadSuccess();
+        }).bind(this);
+        apiGet([vimmaApiVMDetailRoot + this.vmid + '/',
+                vimmaApiAWSVMDetailRoot + '?vm=' + this.vmid],
+                ok, this.loadFail.bind(this));
+    }
+});
