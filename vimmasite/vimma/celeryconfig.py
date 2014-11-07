ecdc651bd71f69179d3724f6067c7f76e020426a
Mihai <mihaiftr@users.noreply.github.com>
2014-11-07 16:58:23 +0200

    celery config: update status every 5 min

--- vimmasite/vimma/celeryconfig.py
+++ vimmasite/vimma/celeryconfig.py
@@ -2,10 +2,11 @@ from celery.schedules import crontab
 
 _every_20s = 20
 _every_1min = crontab(minute='*')
+_every_5min = crontab(minute='*/5')
 
 CELERYBEAT_SCHEDULE = {
     'update-all-vms-status': {
         'task': 'vimma.vmutil.update_all_vms_status',
-        'schedule': _every_20s,
+        'schedule': _every_5min,
     },
 }
