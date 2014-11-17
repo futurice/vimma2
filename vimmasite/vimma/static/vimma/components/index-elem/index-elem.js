b51d9baa20274af45216ca1d81d37acafb12ffdd
Mihai <mihaiftr@users.noreply.github.com>
2014-11-17 20:23:50 +0200

    bootstrap navigation

--- vimmasite/vimma/static/vimma/components/index-elem/index-elem.js
+++ vimmasite/vimma/static/vimma/components/index-elem/index-elem.js
@@ -6,39 +6,30 @@ Polymer('index-elem', {
             {key: 'schedules', title: 'Schedules'},
             {key: 'audit', title: 'Audit (logs)'}
         ];
+        this.tabs.forEach(function(t) {
+            t.url = indexUrl + ajaxFragPrefix + t.key;
+        });
         this.frag = getAjaxFrag();
     },
 
-    domIsReady: false,
-    domReady: function() {
-        this.domIsReady = true;
-        this.updateSelectedIdx();
-    },
-
     observe: {
-        '$.ajaxFrag.head': 'updateSelectedIdx'
+        '$.ajaxFrag.head': 'fragHeadChanged'
     },
 
-    updateSelectedIdx: function() {
-        if (!this.domIsReady) {
-            return;
-        }
+    fragHeadChanged: function() {
         var i, key = this.$.ajaxFrag.head;
         for (i = 0; i < this.tabs.length; i++) {
             if (this.tabs[i].key == key) {
-                this.selectedIdx = i;
                 return;
             }
         }
-        this.frag = this.tabs[0].key;
+        this.async(function() {
+            this.frag = this.tabs[0].key;
+        });
     },
 
-    selectedIdxChanged: function() {
-        var key = this.tabs[this.selectedIdx].key;
-        // Don't overwrite the entire fragment on page load if the tab
-        // doesn't change (i.e. don't replace "vms/3" with "vms").
-        if (this.$.ajaxFrag.head != key) {
-            this.frag = key;
-        }
+    navClicked: function(ev, detail, sender) {
+        ev.preventDefault();
+        this.frag = ev.target.templateInstance.model.tab.key;
     }
 });
